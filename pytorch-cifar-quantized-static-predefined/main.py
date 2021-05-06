628e6088690d7481d7f0af678119e9b46326a7cd
Jonathan Hu <61423654+jonyhu@users.noreply.github.com>
2021-05-05 22:25:20 -0400

    Update main.py

--- pytorch-cifar-quantized-static-predefined/main.py
+++ pytorch-cifar-quantized-static-predefined/main.py
@@ -77,9 +77,9 @@ print('==> Building model..')
 # net = SimpleDLA()
 net = torchvision.models.quantization.resnet18(pretrained=False, quantize=False)
 net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
+# if device == 'cuda':
+#     net = torch.nn.DataParallel(net)
+#     cudnn.benchmark = True
 
 if args.resume:
     # Load checkpoint.
@@ -185,27 +185,3 @@ torch.save(net_static_quantized.state_dict(), 'resnet18_static_quantized_weights
 print_model_size(net_static_quantized)
 
 test(1)
-
-def accuracy(net):
-    global best_acc
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-
-accuracy(net)
-accuracy(net_static_quantized)
