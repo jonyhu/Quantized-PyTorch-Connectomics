597ad5ce0edae2300a3728d0716ea74d8db5ba91
Jonathan Hu <61423654+jonyhu@users.noreply.github.com>
2021-05-05 22:52:36 -0400

    Update main.py

--- pytorch-cifar-quantized-static-predefined/main.py
+++ pytorch-cifar-quantized-static-predefined/main.py
@@ -184,6 +184,8 @@ net_static_quantized = torch.quantization.convert(net_static_quantized, inplace 
 torch.save(net_static_quantized.state_dict(), 'resnet18_static_quantized_weights.pth')
 print_model_size(net_static_quantized)
 
+device = 'cpu'
+
 # Accuracy of non-quantized model
 net.to('cpu')
 test(1)
