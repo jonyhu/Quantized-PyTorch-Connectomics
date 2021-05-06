51247495456c507479ac9b4d255a69d882d1d5bd
Jonathan Hu <61423654+jonyhu@users.noreply.github.com>
2021-05-06 00:02:05 -0400

    Update main_train_noparallel.py

--- pytorch-cifar-quantized-static-predefined/main_train_noparallel.py
+++ pytorch-cifar-quantized-static-predefined/main_train_noparallel.py
@@ -155,7 +155,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+300):
     train(epoch)
     test(epoch)
     scheduler.step()
