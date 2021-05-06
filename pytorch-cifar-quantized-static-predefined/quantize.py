ac21084bd8f7c9652bb8f69efd9dd585536da949
Jonathan Hu <61423654+jonyhu@users.noreply.github.com>
2021-05-05 21:34:43 -0400

    Update quantize.py

--- pytorch-cifar-quantized-static-predefined/quantize.py
+++ pytorch-cifar-quantized-static-predefined/quantize.py
@@ -13,6 +13,11 @@ import argparse
 from models import *
 from utils import progress_bar
 
+def print_model_size(mdl):
+  torch.save(mdl.state_dict(), "tmp.pt")
+  print("%.2f MB" %(os.path.getsize("tmp.pt")/1e6))
+  os.remove('tmp.pt')
+
 net = torchvision.models.quantization.resnet18(pretrained=False, quantize=False)
 net.load_state_dict(torch.load('resnet18_weights_predefined.pth'))
-
+print_model_size(net)
